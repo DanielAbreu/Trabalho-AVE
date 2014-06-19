fdf5a69518914c59fa3c64c2b2d4700fa14daf44
Bruno Fins <bfins@alunos.isel.ipl.pt>
2014-06-19 12:24:54 +0100

    IDataMapper.cs

---
+++ IDataMapper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho_Final_AVE
+{
+    public interface IDataMapper <T>{
+        IEnumerable <T> GetAll(); // Devolve todos os elementos da tabela correspondente 
+        
+        void Update(T val); // Actualiza a linha que tem PK igual à propriedade PK de val (ler cap. Requisitos)
+
+        void Delete(T val); // Apaga a linha com PK igual à propriedade PK de val 
+        
+        void Insert(T val); //Insere uma nova linha com os valores de val e actualiza val com a PK devolvida
+    }
+}
